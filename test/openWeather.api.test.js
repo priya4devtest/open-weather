904810900256a335e02cfe473013d39ac9b2c943
priya k <kpriyadarshini5@gmail.com>
2020-05-19 18:31:13 +1000

    added status code test

--- test/openWeather.api.test.js
+++ test/openWeather.api.test.js
@@ -16,10 +16,15 @@ describe('Open Weather API', function () {
     before('should return the next 5 days forecast', async function () {
         res = await agentOne
         .get(data.url + 'data/2.5/forecast?id=' + data['sydneyCode'] + '&appid=' + data['apiKey']);
-        //console.log(JSON.stringify(res.body));
+        console.log(JSON.stringify(res.body));
         forecasts = res.body.list;
         //console.log("Forecast " + JSON.stringify(forecasts));
       });
+
+    it('should display status as 200', async function()  {
+        expect(res.body['cod']) === '200';
+        console.log("status code is " + res.body['cod']);
+    });
 
     it('should display correct city details', async function()  {
         expect(res.body['city']['id']) === '2147714';
@@ -44,17 +49,17 @@ describe('Open Weather API', function () {
             console.log("Number of days with temp above 18deg is " + countDays.length);
         });
     
-        it('should be able to shortlist rainy days', async function() {
-            for(let forecast of forecasts){
-                if (forecast.weather[0].main === 'Rain')
+    it('should be able to shortlist rainy days', async function() {
+        for(let forecast of forecasts){
+            if (forecast.weather[0].main === 'Rain')
                     rainyDays.push(forecast.dt_txt)
-            }
-            console.log("Days with Rain forecast are " + rainyDays);
-            let countRainDays = [];
-            for (let days of rainyDays){
-                if (countRainDays.includes(days.substring(0,10)) === false)
+        }
+        console.log("Days with Rain forecast are " + rainyDays);
+        let countRainDays = [];
+        for (let days of rainyDays){
+            if (countRainDays.includes(days.substring(0,10)) === false)
                     countRainDays.push(days.substring(0,10));
-            }
-            console.log("Number of days with Rain forecast is " + countRainDays.length);
-            });
+        }
+        console.log("Number of days with Rain forecast is " + countRainDays.length);
+        });
     });
